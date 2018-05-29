e3e89b2ccba1382a3e45135e86e06b5b605ee211
Xiao Shi Liang <shiliang-shelwin.xiao@nokia-sbell.com>
2018-05-29 14:22:59 +0800

    basic function

--- rts.py
+++ rts.py
@@ -1,9 +1,63 @@
 import subprocess
 import re
+from robot.api import ExecutionResult, ResultVisitor
+import sys
+
+class FileDependencyVisitor(ResultVisitor):
+    def __init__(self):
+        self.dependency = {}
+
+    def visit_suite(self, suite):
+        suites = self.get_suites(suite)
+        for suite in suites:
+            self.dependency[suite.name] = self.get_invoked_libraries(suite)
+
+    def get_suites(self, suite):
+        res = [suite]
+        for s in suite.suites:
+            res += self.get_suites(s)
+        return res
+
+    def get_invoked_libraries(self, model):
+        keywords = self.get_invoked_keywords(model)
+        res = []
+        for kw in keywords:
+            if '.' in kw.name and "{" not in kw.name:
+                lib = kw.name.split(".")[0]
+                if lib not in res:
+                    res.append(lib)
+        return res
+
+    def get_invoked_keywords(self, model):
+        res = [model]
+        for kw in model.keywords:
+            res += self.get_invoked_keywords(kw)
+        if hasattr(model, "tests"):
+            for test in model.tests:
+                res += self.get_invoked_keywords(test)
+        if hasattr(model, "suites"):
+            for suite in model.suites:
+                res += self.get_invoked_keywords(suite)
+        return res
 
 class RTS(object):
-    def __init__(self):
-        pass
+    def run(self, cmd):
+        self.dryrun(cmd)
+        output = self._get_output_path(cmd)
+        dependency = self.get_dependency(output)
+        changes = self.get_changed_files()
+        affected = self.get_affected_suites(dependency, changes)
+        cmd = self.get_updated_cmd(cmd, affected)
+        subprocess.check_output(cmd, shell=True)
+
+    def get_updated_cmd(self, cmd, suites):
+        origin_suites = re.findall("-s\s\S*", cmd)
+        for s in origin_suites:
+            cmd = cmd.replace(s, "")
+        append = ""
+        for s in suites:
+            append += "-s '%s'" % s
+        return cmd[0:6] + append + cmd[6:]
 
     def dryrun(self, cmd):
         cmd = cmd[0:6] + "--dryrun " + cmd[6:]
@@ -26,6 +80,25 @@ class RTS(object):
         return directory + '/' + name
 
     def get_dependency(self, output):
-        dependency = {}
-        return dependency
+        result = ExecutionResult(output)
+        visitor = FileDependencyVisitor()
+        result.visit(visitor)
+        return visitor.dependency
 
+    def get_changed_files(self, cmd="git diff HEAD HEAD~ --name-only"):
+        result = subprocess.check_output(cmd, shell=True)
+        result = result.strip().split('\n')
+        return [name.split('/')[-1].split(".")[0] for name in result if name.startswith('test/ET')]
+
+    def get_affected_suites(self, dependency, changes):
+        res = {}
+        for suite in dependency:
+            if set(dependency[suite]).intersection(changes):
+                res[suite] = True
+            else:
+                res[suite] = False
+        return res
+
+if __name__ == "__main__":
+    rts = RTS()
+    rts.run(sys.argv[1])
--- test_rts.py
+++ test_rts.py
@@ -9,6 +9,29 @@ class TestRTS(unittest.TestCase):
     def test_get_output_path(self):
         cmd = "pybot -d ./log -o o.xml ."
         self.assertEqual(self.rts._get_output_path(cmd), "./log/o.xml")
+        cmd = "pybot ."
+        self.assertEqual(self.rts._get_output_path(cmd), "./output.xml")
+        cmd = "pybot -o o.xml ."
+        self.assertEqual(self.rts._get_output_path(cmd), "./o.xml")
+        cmd = "pybot -d ./log ."
+        self.assertEqual(self.rts._get_output_path(cmd), "./log/output.xml")
+
+    def test_get_dependency(self):
+        output = './testdata/output.xml'
+        self.assertNotEqual(self.rts.get_dependency(output), {})
+
+    def test_get_affected_suites(self):
+        dependency = {"ET":["NbsLibrary", "common_lib"]}
+        changes = ["common_lib"]
+        self.assertEqual(self.rts.get_affected_suites(dependency, changes), {"ET":True})
+        dependency = {"ET":["NbsLibrary", "common_lib2"]}
+        changes = ["common_lib"]
+        self.assertEqual(se lf.rts.get_affected_suites(dependency, changes), {"ET":False})
+
+    def test_get_updated_cmd(self):
+        cmd = "pybot -s 'FM' -s 'CM' ."
+        suites = ["FM_2"]
+        self.assertEqual(self.rts.get_updated_cmd(cmd, suites), "pybot -s 'FM_2'  .")
 
 if __name__ == '__main__':
     unittest.main()
